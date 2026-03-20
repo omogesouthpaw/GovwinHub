8678efd70fcf1ed51bef5927771a413ac3db10d6
omogesouthpaw <omogesouthpaw@gmail.com>
2026-03-20 09:03:46 +0100

    Remove dist from tracking

--- dist/database/datasource.d.ts
+++
@@ -1,9 +0,0 @@
-import { Knex } from 'knex';
-export interface KnexConnectionOptions {
-    host: string;
-    port: number;
-    user: string;
-    password: string;
-    database: string;
-}
-export declare function createKnexInstance(options: KnexConnectionOptions): Knex;
--- dist/logger/interfaces/logger.interface.d.ts
+++
@@ -1,5 +0,0 @@
-export interface LoggerOptionsInterface {
-    includeUserAgent?: boolean;
-    sensitiveFields?: string[];
-    logLevel?: 'debug' | 'info' | 'warn' | 'error';
-}
