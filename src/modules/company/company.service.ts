d2b815df637c3ae478b20b201ad4b884f0e9959e
omogesouthpaw <omogesouthpaw@gmail.com>
2026-03-25 18:59:19 +0100

    changed from absolute path to relative path

--- src/modules/company/company.service.ts
+++ src/modules/company/company.service.ts
@@ -8,8 +8,7 @@ import { CreateOrganizationDto, IOrganization, UpdateOrganizationDto } from './i
 // import { KNEX_CONNECTION } from '../database';
 // import { BaseRepository, mapToCamelCase } from '../database/base.repository';
 import { CreateCompanyDto, UpdateCompanyDto } from './dto/company.dto';
-import { ICompany } from 'src/common/interfaces/company.interface';
-// import { ICompany } from 'src/interfaces';
+import { ICompany } from '../../common/interfaces/company.interface';
 
 @Injectable()
 export class CompanyService extends BaseRepository<ICompany> {
